6d048f0b5f1730b51de2d67094b6350d808f1c3e
KAMKAZEMARCI <74118308+KAMKAZEMARCI@users.noreply.github.com>
2021-02-18 11:59:21 +0100

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,12 +1,12 @@
-import pyautogui, time
-from time import sleep
+import pyautogui, time #imports pyautogui and time modules
+from time import sleep #imports sleep from time module
 
 ##########
-count = 0#
+count = 0#  #counter
 ##########
 
 while True:
-    time.sleep(10)
-    count += 1 
-    myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(r'Screenshot\screenshot{}.png'.format(count))
+    time.sleep(10) #you can change the time if u want (second)
+    count += 1 #Don't change this
+    myScreenshot = pyautogui.screenshot() #Takes the screenshot
+    myScreenshot.save(r'path here\screenshot{}.png'.format(count)) #saves the screenshot, change "path here".
